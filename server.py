c1f4af36e3dbc20db41e286e2ee3939aa2a3817f
Hung Nguyen <hungnt89@gmail.com>
2020-07-13 01:22:44 +0700

    Handle errors better! Not conn list yet!

--- server.py
+++ server.py
@@ -9,23 +9,32 @@ PORT = 5656
 ADDR = (HOST, PORT)
 
 BLACK_LIST = ['192.168.1.26']
+ACTIVE_CLIENTS = []
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
+def addr_to_str(addr):
+    x = ':'.join(map(str, addr))
+    return x
+
+
 def receive(conn):
     if not isClosed(conn):
         # print('receive runs!')
-        username = conn.recv(HEADER).decode(ENCODING).strip()
-        get_length = conn.recv(HEADER).decode(ENCODING)
-        # if msg_length:
-        msg_length = int(get_length)
-        msg = conn.recv(msg_length).decode(ENCODING)  # Het msg.
-        # print('End Receive func.')
-        return (username, msg)
-    return ('No user', 'No msg')
+        try:
+            username = conn.recv(HEADER).decode(ENCODING).strip()
+            get_length = conn.recv(HEADER).decode(ENCODING)
+            msg_length = int(get_length)
+            msg = conn.recv(msg_length).decode(ENCODING)  # Het msg.
+            return (username, msg)
+
+        except ConnectionResetError:
+            conn.close()
+            print("[CLIENT-SIDE] Connection lost unexpectedly!")
+            return conn
 
 
 def handle_client(conn, addr):
@@ -33,18 +42,27 @@ def handle_client(conn, addr):
     print(f"\n[NEW CONNECTION] {addr} CONNECTED!")
 
     while True:
-        username, msg = receive(conn)
+        if isClosed(conn):
+            break
+        try:
+            username, msg = receive(conn)
+        except (ValueError, TypeError):
+            break
         if msg == DISCONNECT_MSG:
             break
         print(f"[ {username} ] {msg}")
 
-    print(f"-- {username} -- has exited!")
+    ACTIVE_CLIENTS.pop()
+    try:
+        print(f"-- {username} -- has exited!")
+    except UnboundLocalError:
+        print('-- [Client] -- has exited! Unknown username due to no first message.')
+
     conn.close()
+    return (conn, addr)
 
 
 def start():
-    print(f"[SERVER] Server is listening on :{PORT}")
-    print('-----------------------------------------')
     server.listen()
 
     while True:
@@ -54,16 +72,22 @@ def start():
             conn.send(ABORTED_MSG.encode(ENCODING))
             conn.close()
         else:
+            ACTIVE_CLIENTS.append(conn)
             conn.send(ACCEPTED_MSG.encode(ENCODING))
         # Tao 1 thread cho moi client.
         if not isClosed(conn):
             thread = threading.Thread(
                 target=handle_client, args=(conn, client_addr))
             thread.start()
+        else:
+            ACTIVE_CLIENTS.pop()
 
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}\n")
+        print(f"\n[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f'[CONN LIST] {len(ACTIVE_CLIENTS)}')
+        print('-----------------------------------------')
 
 
 if __name__ == '__main__':
-    print(f"\n[SERVER] Server is starting...")
+    print(f"\n[SERVER] Server is listening on {HOST}:{PORT}")
+    print('-----------------------------------------')
     start()
